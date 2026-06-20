182b699b020dc217286ef42e282984f4c1c79097
Saurrabhh <saurabhkyadav99@gmail.com>
2026-06-20 16:31:43 +0530

    url added

--- internal/app/root.go
+++ internal/app/root.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
@@ -13,15 +14,24 @@ type rootResponse struct {
 	Environment string `json:"environment"`
 	Status      string `json:"status"`
 	Docs        string `json:"docs"`
+	Health        string `json:"health"`
 }
 
 func (app *Application) rootHandler(w http.ResponseWriter, r *http.Request) {
+	scheme := "http"
+	if r.TLS != nil || r.Header.Get("X-Forwarded-Proto") == "https" {
+		scheme = "https"
+	}
+	healthURL := fmt.Sprintf("%s://%s/health", scheme, r.Host)
+	docsURL := fmt.Sprintf("%s://%s/docs", scheme, r.Host)
+
 	res := rootResponse{
 		Name:        "Splittr API",
 		Version:     "1.0.0",
 		Environment: os.Getenv("APP_ENV"),
 		Status:      "healthy",
-		Docs:        "/health", // This can be updated to point to a documentation route (like /docs or Swagger) later.
+		Docs:        docsURL,
+		Health:      healthURL,
 	}
 
 	response.JSON(w, http.StatusOK, res)
